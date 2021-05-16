090cbca46c10b06a2d158a8bb79c096815ba6781
ulugbekwebp <ulugbekwebp@gmail.com>
2021-05-16 11:01:18 +0500

    jamol

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 var jamol = document.querySelector(".jamol");
 var header_jamol = document.querySelector(".header-jamol");
 var header_right = document.querySelector(".header-right");
+var main_bottom = document.querySelector(".main-bottom");
 
 jamol.addEventListener("click", function(){
   header_jamol.classList.toggle('header-jamol-active');
   header_right.classList.toggle('header-right-active');
+  main_bottom.classList.toggle('main-bottom-active');
 });
